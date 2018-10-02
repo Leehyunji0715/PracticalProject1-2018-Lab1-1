2a1c14e42544c521a9975c82c22fd48bce5c761b
Leehyunji0715 <leehyunji0715@gamil.com>
2018-10-02 21:08:43 +0900

    end

--- src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
+++ src/test/java/edu/handong/csee/pp1/bigdata/lab1_1/CalculatorTest.java
@@ -30,7 +30,7 @@ public class CalculatorTest {
 	public void testMod() {
 		Calculator myCalc = new Calculator();
 		assertEquals(myCalc.mod(2, 3),2);
-	}
+	}   
 
 	@Test
 	public void testMultiply() {
